f8da042a228c7fb25bb8df6ed5d9acd86bcb2e30
Swati <4evr.swati@gmail.com>
2023-08-05 19:22:09 +0530

    Database Connected

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -1,2 +1,11 @@
 const mongoose = require('mongoose')
 
+const connectDB = () => {
+    return mongoose.connect(process.env.DB_URL).then((x) => {
+        console.log('Database Connected Successfully');
+    }).catch((err) => {
+        console.log('Database not connected');
+    })
+}
+
+module.exports = connectDB;
--- index.js
+++ index.js
@@ -1,13 +1,15 @@
-const express = require('express')
+const express = require('express');
+const connectDB = require('./config/dbConnect');
 const app = express()
 
 require('dotenv').config();
+const PORT = process.env.PORT
+connectDB();
 
 app.get('/', (req, res) => {
     res.send('Hello Panda');
 })
 
-PORT = process.env.PORT
 
 app.listen(PORT, () => {
     console.log(`Server is listening at port ${PORT}`);
